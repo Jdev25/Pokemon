1833e2139a107ef8061d2208c8150cab6f7f5f19
Estudiante <estudiante@Mac17.local>
2025-11-10 11:33:01 -0500

    Fix: corrected cardDeck.css import case

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./components/css/main.css";
-import "./components/CardDeck.css";
+import "./components/cardDeck.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
